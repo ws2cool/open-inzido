875be80c216036ec5d5a144bf247b3f978e691ac
Chanseok Park <pcspapa@gmail.com>
2019-08-20 18:05:38 +0900

    first commit

---
+++ src/main/java/com/makesomenoise/open/inzido/OpenInzidoApplication.java
@@ -0,0 +1,13 @@
+package com.makesomenoise.open.inzido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenInzidoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenInzidoApplication.class, args);
+  }
+
+}
